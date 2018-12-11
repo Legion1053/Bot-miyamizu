b36027c6958d42e09ed278de71c430587f3a0e0c
nguyenthangleader15 <38281820+nguyenthangleader15@users.noreply.github.com>
2018-12-11 09:35:56 +0700

    Update userinfo

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,16 +1,17 @@
+const Discord= require("discord.js" );
 module.exports.run = async(bot,message,args) =>{
-	message.channel.send(userInfo(message.author));
+	let userCreated = message.author.createdAt.toString().split(' ');
+	let userJoined = message.channel.guild.joinedAt.toString().split(' ');
+	let embed = new Discord.RichEmbed()
+		.addField("User name",message.author.username)
+		.addField("ID",message.author.id)
+		.addField("Created at",userCreated[1] +','+userCreated[2]+' '+userCreated[3])
+		.addField("Joined server at",userJoined[1] +','+userJoined[2]+' '+userJoined[3])
+		.setColor("#3399ff")
+		.setThumbnail(message.author.avatarURL)
+	message.channel.sendEmbed(embed);
 }
 
 module.exports.config = {
 	command: "userinfo"
-}
-
-let userInfo = user =>{
-	let finalString= ' ';
-
-	finalString += '**'+user.username+'**, with the ID of **'+user.id +'**';
-	let userCreated = user.createdAt.toString().split(' ');
-	finalString += ' and was created at: **' + userCreated[1] +','+userCreated[2]+' '+userCreated[3]+'**';
-	return finalString;
 }
