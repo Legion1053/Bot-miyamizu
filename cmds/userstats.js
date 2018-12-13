0edd868924763c2d548f62a8a682459917a18fe7
nguyenthangleader15 <38281820+nguyenthangleader15@users.noreply.github.com>
2018-12-13 13:51:17 +0700

    Update choose.js

--- cmds/userstats.js
+++ cmds/userstats.js
@@ -1,20 +1,19 @@
 const fs = require('fs');
-const Discord = require("discord.js");
-const userData = JSON.parse(fs.readFileSync("Storage/userData.json"));
+const userData = JSON.parse(fs.readFileSync("Storage/userData.json","utf8"));
 
 module.exports.run = async(bot,message,args) =>{
 	let sender= message.author;
 	//Make sure their username is there before write the file
-	if(!userData[sender.id]) userData[sender.id] = {//To make the ID unique when they are in the diffrent server
+	if(!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {//To make the ID unique when they are in the diffrent server
 		messagesSent: 0
 	}
 	//Increase messages Sent
-	userData[sender.id].messagesSent++;
+	userData[sender.id + message.guild.id].messagesSent++;
 	//Save the file
 	fs.writeFile('Storage/userData.json',JSON.stringify(userData), err =>{
 		if(err) console.error(err);// If their is a error it will log this code
 	})
-	message.channel.send('You have sent **'+userData[sender.id].messagesSent+'** messages');
+	message.channel.send('You have sent **'+userData[sender.id+message.guild.id].messagesSent+'** messages');
 }
 module.exports.config = {
 	command: "userstats"
