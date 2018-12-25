4551d2252a995912fada55b22180d2f3215a8e63
nguyenthangleader15 <38281820+nguyenthangleader15@users.noreply.github.com>
2018-12-25 16:57:41 +0700

    Added Actions

---
+++ cmds/Actions/hug.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async(bot,message,args) =>{
+	let {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/hug`);
+    let mention = message.mentions.users.first();
+    if (!mention) return message.channel.send(`Bạn phải chọn người để ôm đã :|`).then(msg => {
+    	msg.delete(3000)
+  	});
+    else if(args[0] === '<@514823687560757268>' ) {
+    	message.channel.send('Aww cảm ơn bạn XD');	
+    	message.channel.send(body.url);
+    }  
+    else {
+    	message.channel.send(`**${message.author.username}** vừa ôm **${mention.username}** kìa :hearts:`);
+    	message.channel.send(body.url);
+    } 
+}
+
+module.exports.config = {
+	command: 'hug'
+}
