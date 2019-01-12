ae5ffc2145ab0a176d201405ef0c6ffd8da4f00d
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-01-12 16:08:44 +0700

    Update idk.js

--- cmds/Actions/idk.js
+++ cmds/Actions/idk.js
@@ -2,7 +2,7 @@ const superagent = require("superagent");
 
 module.exports.run = async(bot,message,args) =>{ 
 	let {body} = await superagent
-    	.get(`https://api.tenor.com/v1/random?q=anime+shrug&key=10LA8OYRLBFU&limit=1`);
+    	.get(`https://api.tenor.com/v1/random?q=anime+shrug&key=${process.env.TENOR_API}&limit=1`);
     
     message.channel.send(`¯\\_\(ツ)\_\/¯ ${body.results[0].media[0].gif.url}`);
 }
