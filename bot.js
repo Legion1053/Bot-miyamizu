30569bf5088d573a6ec19aae1ae8609848aafbd8
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-01-12 16:16:08 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-const client = require("./botsettings.json");
+//const client = require("./botsettings.json");
 // Import the discord.js module
 const Discord = require("discord.js");
-const prefix = client.prefix;
+const prefix = '>>';
 const bot = new Discord.Client();
 const fs = require('fs');
 const ytdl = require('ytdl-core');
@@ -76,4 +76,4 @@ bot.on("guildMemberAdd",member =>{
 bot.on("guildMemberRemove",member =>{
 	member.guild.channels.find("name","chat-room").sendMessage(`${member.user.username}, Tạm biệt bạn nhé...`);
 });
-bot.login(client.token);
+bot.login(process.env.BOT_TOKEN);
