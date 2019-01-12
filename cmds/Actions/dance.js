66c626f67b9f8c5bbaae4a93165f8b11e7d46f91
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-01-12 16:07:20 +0700

    Update dance.js

--- cmds/Actions/dance.js
+++ cmds/Actions/dance.js
@@ -2,7 +2,7 @@ const superagent = require("superagent");
 
 module.exports.run = async(bot,message,args) =>{ 
 	let {body} = await superagent
-    	.get(`https://api.tenor.com/v1/random?q=anime+dance&key=10LA8OYRLBFU&limit=1`);
+    	.get(`https://api.tenor.com/v1/random?q=anime+dance&key=${process.env.TENOR_API}&limit=1`);
     let mention = message.mentions.users.first();
     if(args[0] === '<@514823687560757268>') message.channel.send(`Nhảy nào XD ${body.results[0].media[0].gif.url}`);	
     message.channel.send(`**${message.author.username}** đang nhảy kìa XD ${body.results[0].media[0].gif.url}`);
