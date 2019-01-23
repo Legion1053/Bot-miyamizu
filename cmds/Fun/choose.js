29dc49a9b1b4081f8b89feb589f79e1bb0e62a8f
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-01-23 17:16:48 +0700

    Update choose.js

--- cmds/Fun/choose.js
+++ cmds/Fun/choose.js
@@ -1,11 +1,10 @@
-const Discord= require("discord.js" );
-module.exports.run= async(bot,message,args)=>
-{
-	 //let choose= args.toString().split(' ');
+module.exports.run= async(bot,message,args)=>{
+	 let choose= args.toString().split(' ');
 	 let reply= Math.floor(Math.random()*(args.length)); 
 	 message.channel.send(`Tui chọn ${args[reply]} :grin:`);
 }
 
 module.exports.config ={
-	command: "choose"
+	command: "choose",
+  	category: "Fun"
 }
