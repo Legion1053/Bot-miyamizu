a99e13c26d9bce4fb68189200664ec26b36877bf
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-01-23 17:11:01 +0700

    Update help.js

--- cmds/Info/help.js
+++ cmds/Info/help.js
@@ -1,7 +1,16 @@
-module.exports.run = async(bot,message,args,db) =>{
+const Discord = require('discord.js');
+
+module.exports.run = async(bot,message,args) =>{
 	try {
-        message.channel.send(`Commands: \n\n${bot.commands.map(cmd => `\`${cmd.config.command}\``).join(" ")}`);
+        let help = new Discord.RichEmbed()
+          .setAuthor('Danh sách các câu lệnh')
+          .addField('Info',`${bot.commands.filter(cmd => cmd.config.category === 'Info').map(cmd => `\`${cmd.config.command}\``).join(" ")} `)
+          .addField('Fun',`${bot.commands.filter(cmd => cmd.config.category === 'Fun').map(cmd => `\`${cmd.config.command}\``).join(" ")} `)
+          .addField('Actions',`${bot.commands.filter(cmd => cmd.config.category === 'Actions').map(cmd => `\`${cmd.config.command}\``).join(" ")} `)
+          .addField('Economy',`${bot.commands.filter(cmd => cmd.config.category === 'Economy').map(cmd => `\`${cmd.config.command}\``).join(" ")} `)
+        message.channel.send(help);
     } catch (e) {
+        message.channel.send('Không thể gửi được câu lệnh này! ');
         throw e;
     }
 }
