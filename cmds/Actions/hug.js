0692e86bf2130cb165f073993374ea26217174a2
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-01 15:53:16 +0700

    Update hug.js

--- cmds/Actions/hug.js
+++ cmds/Actions/hug.js
@@ -4,14 +4,19 @@ const superagent = require("superagent");
 module.exports.run = async(bot,message,args) =>{
 	let {body} = await superagent
     .get(`https://nekos.life/api/v2/img/hug`);
-    let mention = message.mentions.users.first();
-    if (!mention) return message.channel.send(`Bạn phải chọn người để ôm đã :|`).then(msg => {
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.url);
+  let mention = message.mentions.users.first();
+  if (!mention) return message.channel.send(`Bạn phải chọn người để ôm đã :|`).then(msg => {
     	msg.delete(3000)
-  	});
-    else if(args[0] === '<@514823687560757268>') message.channel.send(`Aww cảm ơn bạn XD ${body.url}`);	    
-    else message.channel.send(`**${message.author.username}** vừa ôm **${mention.username}** kìa :hearts: ${body.url}`);
+  });
+  else if(args[0] === '<@514823687560757268>') message.channel.send(`Aww cảm ơn bạn XD`,{embed: Emb});	    
+  else message.channel.send(`**${message.author.username}** vừa ôm **${mention.username}** kìa :hearts:`,{embed: Emb});
 }
 
 module.exports.config = {
-	command: 'hug'
+	command: 'hug',
+  	category: 'Actions',
+  	description: 'Dùng để ôm ai đó <3',
+  	usage: '>>hug [@user]'
 }
