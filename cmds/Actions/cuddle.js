189e004ebb97cf993dd4a58ea72f3f08c9fbe7c7
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-01 15:50:43 +0700

    Update cuddle.js

--- cmds/Actions/cuddle.js
+++ cmds/Actions/cuddle.js
@@ -4,13 +4,21 @@ const superagent = require("superagent");
 module.exports.run = async(bot,message,args) =>{
 	let {body} = await superagent
     	.get(`https://nekos.life/api/v2/img/cuddle`);
-    let mention = message.mentions.users.first();
-    if (!mention) return message.channel.send(`Bạn phải chọn người để âu yếm đã :3`).then(msg => {
+  let mention = message.mentions.users.first();  
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.url);
+  if (!mention) return message.channel.send(`Bạn phải chọn người để âu yếm đã :3`).then(msg => {
     	msg.delete(3000)
-  	});
-    else if(args[0] === '<@514823687560757268>') message.channel.send(`Nào ngoan ngoan, **${message.author.username}** :hearts: ${body.url}`);	
-    else message.channel.send(`**${message.author.username}** vừa âu yếm **${mention.username}** kìa :3 ${body.url}`);
+  });
+  else if(args[0] === '<@514823687560757268>') {
+      message.channel.send(`Nào ngoan ngoan, **${message.author.username}** :hearts:`,{embed: Emb});
+  } else {
+      message.channel.send(`**${message.author.username}** vừa âu yếm **${mention.username}** kìa :3`,{embed: Emb});
+  }
 }
 module.exports.config = {
-	command: 'cuddle'
+	command: 'cuddle',
+  	category: 'Actions',
+  	description: 'Dùng để âu yếm ai đó :D',
+  	usage: '>>cuddle [@user]'
 }
