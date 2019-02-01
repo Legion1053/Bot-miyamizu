b40ebf5beb312eeea3b2f232999dd6fd23eb3cf7
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-01 15:51:51 +0700

    Update feed.js

--- cmds/Actions/feed.js
+++ cmds/Actions/feed.js
@@ -4,14 +4,19 @@ const superagent = require("superagent");
 module.exports.run = async(bot,message,args) =>{
 	let {body} = await superagent
     	.get(`https://nekos.life/api/v2/img/feed`);
-    let mention = message.mentions.users.first();
-    if (!mention) return message.channel.send(`Bạn phải chọn người để cho ăn đã :3`).then(msg => {
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.url);
+  let mention = message.mentions.users.first();
+  if (!mention) return message.channel.send(`Bạn phải chọn người để cho ăn đã :3`).then(msg => {
     	msg.delete(3000)
-  	});
-    else if(args[0] === '<@514823687560757268>') message.channel.send(`Yummy Yummy :yum: ${body.url}`);	  
-    else message.channel.send(`**${message.author.username}** vừa cho bạn ăn **${mention.username}** kìa :3 ${body.url}`); 
+  });
+  else if(args[0] === '<@514823687560757268>') message.channel.send(`Yummy Yummy :yum:`,{embed: Emb});	  
+  else message.channel.send(`**${message.author.username}** vừa cho **${mention.username}** ăn kìa :3`,{embed: Emb}); 
 }
 
 module.exports.config = {
-	command: 'feed'
+	command: 'feed',
+  category: 'Actions',
+  description: 'Dùng để cho ai đó ăn :3',
+  usage: '>>feed [@user]'
 }
