d9c2d11cced2a02dadfd111ca64e7a289bfaf61d
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-01 15:52:26 +0700

    Update holdhand.js

--- cmds/Actions/holdhand.js
+++ cmds/Actions/holdhand.js
@@ -1,16 +1,22 @@
+const Discord = require('discord.js');
 const superagent = require("superagent");
 
 module.exports.run = async(bot,message,args) =>{ 
 	let {body} = await superagent
     	.get(`https://api.tenor.com/v1/random?q=anime+hold+hands&key=${process.env.TENOR_API}&limit=1`);
-    let mention = message.mentions.users.first();
-    if (!mention) return message.channel.send(`Bạn phải chọn người để nắm tay đã :3`).then(msg => {
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.results[0].media[0].gif.url);
+  let mention = message.mentions.users.first();
+  if (!mention) return message.channel.send(`Bạn phải chọn người để nắm tay đã :3`).then(msg => {
     	msg.delete(3000)
-  	});
-    else if(args[0] === '<@514823687560757268>') message.channel.send(`XD \n ${body.results[0].media[0].gif.url}`);	
-    else message.channel.send(`**${message.author.username}** vừa nắm tay **${mention.username}** kìa :heart: ${body.results[0].media[0].gif.url}`);
+  });
+  else if(args[0] === '<@514823687560757268>') message.channel.send(`XD `,{embed: Emb});	
+  else message.channel.send(`**${message.author.username}** vừa nắm tay **${mention.username}** kìa :heart:`,{embed: Emb});
 }
 
 module.exports.config ={
-	command: 'holdhand'
+	command: 'holdhand',
+  category: 'Actions',
+  description: 'Dùng để nắm tay ai đó',
+  usage: '>>holdhand [@user]'
 }
