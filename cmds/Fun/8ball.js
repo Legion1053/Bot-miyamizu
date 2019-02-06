cbcb10ffc6df1ad5f0517f624862afc7cdf541a8
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-06 21:22:00 +0700

    Update 8ball.js

--- cmds/Fun/8ball.js
+++ cmds/Fun/8ball.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(bot,message,args) =>{
-	let replies = ['Ừ, chắc chắn rồi','Không bao giờ ','Bạn thử hỏi lại xem','Có thể đó','Ừ,cũng có thể','Chưa chắc chắn lắm'];
+	let replies = ['Ừ, chắc chắn rồi','Không bao giờ ','Bạn thử hỏi lại xem','Có thể đó','Ừ,cũng có thể','Tốt nhất không nên nói cho bạn'];
 	let question = args.slice().join(" ");
 	let result = Math.floor(Math.random()*replies.length);
-	let embed = new Discord.RichEmbed()
-		.addField(`:speech_balloon: ${replies[result]}. :8ball:`)
-		.setColor('RANDOM');
-	message.channel.send(embed);
+	
+	message.channel.send(`:speech_balloon: Trả lời, ${replies[result]}. :8ball:`);
 }
 
 module.exports.config = {
 	command: '8ball',
-  	category: 'Fun'
+  category: 'Fun',
+  description: "Dùng để trả lời câu hỏi ",
+  usage: ">>8ball [câu hỏi]"
 }
