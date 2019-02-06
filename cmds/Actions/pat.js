1729476bcfee5bd139f57fe7da0c347e7cfce7ea
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-06 20:59:50 +0700

    Update pat.js

--- cmds/Actions/pat.js
+++ cmds/Actions/pat.js
@@ -4,14 +4,19 @@ const superagent = require("superagent");
 module.exports.run = async(bot,message,args) =>{
 	let {body} = await superagent
     	.get(`https://nekos.life/api/v2/img/pat`);
-    let mention = message.mentions.users.first();
-    if (!mention) return message.channel.send(`Bạn phải chọn người để xoa đầu đã :3`).then(msg => {
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.url);
+  let mention = message.mentions.users.first();
+  if (!mention) return message.channel.send(`Bạn phải chọn người để xoa đầu đã :3`).then(msg => {
     	msg.delete(3000)
-  	});
-    else if(args[0] === '<@514823687560757268>') message.channel.send(`Aww cảm ơn bạn nha :3 ${body.url}`);	
-    else message.channel.send(`**${message.author.username}** vừa xoa đầu **${mention.username}** kìa :innocent: ${body.url}`);
+  });
+  else if(args[0] === '<@514823687560757268>') message.channel.send(`Aww cảm ơn bạn nha :3`,{embed: Emb});	
+  else message.channel.send(`**${message.author.username}** vừa xoa đầu **${mention.username}** kìa :innocent: `,{embed: Emb});
 }
 
 module.exports.config = {
-	command: 'pat'
+	command: 'pat',
+  	category: 'Actions',
+  	description: 'Dùng để xoa đầu ai đó XD',
+  	usage: '>>pat [@user]'
 }
