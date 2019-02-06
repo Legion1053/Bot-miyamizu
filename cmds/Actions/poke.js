e4819af03495200bc0859ecf8aa580c11e51f3d7
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-06 21:00:43 +0700

    Update poke.js

--- cmds/Actions/poke.js
+++ cmds/Actions/poke.js
@@ -4,14 +4,19 @@ const superagent = require("superagent");
 module.exports.run = async(bot,message,args) =>{
 	let {body} = await superagent
     	.get(`https://nekos.life/api/v2/img/poke`);
-    let mention = message.mentions.users.first();
-    if (!mention) return message.channel.send(`Bạn phải chọn người để chọc đã :3`).then(msg => {
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.url);
+  let mention = message.mentions.users.first();
+  if (!mention) return message.channel.send(`Bạn phải chọn người để chọc đã :3`).then(msg => {
     	msg.delete(3000)
-  	});
-    else if(args[0] === '<@514823687560757268>') message.channel.send(`Heyyyyy :< ${body.url}`);	
-    else message.channel.send(`**${message.author.username}** vừa chọc **${mention.username}** kìa :innocent: ${body.url}`);
+  });
+  else if(args[0] === '<@514823687560757268>') message.channel.send(`Heyyyyy :< `,{embed: Emb});	
+  else message.channel.send(`**${message.author.username}** vừa chọc **${mention.username}** kìa :innocent:`,{embed: Emb});
 }
 
 module.exports.config = {
-	command: 'poke'
+	command: 'poke',
+  	category: 'Actions',
+  	description: 'Dùng để chọc ai đó :3',
+  	usage: '>>poke [@user]'
 }
