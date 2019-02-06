501e9474b6cd954638e7d408291c832ab09fec23
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-06 21:08:00 +0700

    Update tickle.js

--- cmds/Actions/tickle.js
+++ cmds/Actions/tickle.js
@@ -4,14 +4,19 @@ const superagent = require("superagent");
 module.exports.run = async(bot,message,args) =>{
 	let {body} = await superagent
     	.get(`https://nekos.life/api/v2/img/tickle`);
-    let mention = message.mentions.users.first();
+  let mention = message.mentions.users.first();
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.url);
     if (!mention) return message.channel.send(`Bạn phải chọn người để "cù" đã :3`).then(msg => {
     	msg.delete(3000)
   	});
-    else if(args[0] === '<@514823687560757268>' ) message.channel.send(`*Hi hi hi nhột quá* :joy: ${body.url}`);	  
-    else message.channel.send(`**${message.author.username}** vừa "cù" **${mention.username}** kìa :joy: ${body.url}`);
+    else if(args[0] === '<@514823687560757268>' ) message.channel.send(`*Hi hi hi nhột quá* :joy: `,{embed: Emb});	  
+    else message.channel.send(`**${message.author.username}** vừa "cù" **${mention.username}** kìa :joy: `,{embed: Emb});
 }
 
 module.exports.config = {
-	command: 'tickle'
+	command: 'tickle',
+  	category: 'Actions',
+  	description: 'Dùng để cù ai đó :3',
+  	usage: '>>tickle [@user]'
 }
