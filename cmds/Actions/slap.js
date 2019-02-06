1ecfbb16bb11ea21d6dcdc16e5dcbc1384c8ff88
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-06 21:04:17 +0700

    Update slap.js

--- cmds/Actions/slap.js
+++ cmds/Actions/slap.js
@@ -1,17 +1,22 @@
-const Discord = require("discord.js");
+onst Discord = require("discord.js");
 const superagent = require("superagent");
 
-module.exports.run = async(bot,message,args) =>{
+module.exports.run = async(bot,message,args,db) =>{
 	let {body} = await superagent
     	.get(`https://nekos.life/api/v2/img/slap`);
-    let mention = message.mentions.users.first();
-    if (!mention) return message.channel.send(`Bạn phải chọn người để slap đã :v`).then(msg => {
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.url);
+  let mention = message.mentions.users.first();
+  if (!mention) return message.channel.send(`Bạn phải chọn người để tát đã :v`).then(msg => {
     	msg.delete(3000)
-  	});
-    else if(args[0] === '<@514823687560757268>') message.channel.send(`**:(** ${body.url}`);	
-    else message.channel.send(`**${message.author.username}** vừa tát **${mention.username}** kìa :cold_sweat: ${body.url}`);
+  });
+  else if(args[0] === '<@514823687560757268>') message.channel.send(`**:(** `,{embed: Emb});	
+  else message.channel.send(`**${message.author.username}** vừa tát **${mention.username}** kìa :cold_sweat:`,{embed: Emb});
 }
 
 module.exports.config = {
-	command: 'slap'
+	command: 'slap',
+ 	category: 'Actions',
+  	description: 'Dùng để tát ai đó :v',
+  	usage: '>>slap [@user]'
 }
