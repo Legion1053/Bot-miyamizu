ae4a279b05cdce6df8e67292411738b89e419322
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-06 21:01:21 +0700

    Update shrug.js

--- cmds/Actions/shrug.js
+++ cmds/Actions/shrug.js
@@ -1,12 +1,17 @@
+const Discord = require('discord.js');
 const superagent = require("superagent");
 
-module.exports.run = async(bot,message,args) =>{ 
+module.exports.run = async(bot,message,args,db) =>{ 
 	let {body} = await superagent
-    	.get(`https://api.tenor.com/v1/random?q=anime+shrug&key=${process.env.TENOR_API}&limit=1`);
-    
-    message.channel.send(`¯\\_\(ツ)\_\/¯ ${body.results[0].media[0].gif.url}`);
+    	.get(`https://api.tenor.com/v1/random?q=anime+shrug&key=10LA8OYRLBFU&limit=1`);
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.results[0].media[0].gif.url); 
+  message.channel.send(`¯\\_\(ツ)\_\/¯ `,{embed: Emb});
 }
 
 module.exports.config ={
-	command: 'shrug'
+	command: 'shrug',
+  category: 'Actions',
+  description: `Dùng để ¯\\_\(ツ)\_\/¯`,
+  usage: '>>shrug'
 }
