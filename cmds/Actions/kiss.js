a72dbb68ffdad7da9e61ca0e230caf8985972404
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-06 20:33:08 +0700

    Update kiss.js

--- cmds/Actions/kiss.js
+++ cmds/Actions/kiss.js
@@ -4,15 +4,20 @@ const superagent = require("superagent");
 module.exports.run = async(bot,message,args) =>{
 	let {body} = await superagent
 		.get(`https://nekos.life/api/v2/img/kiss`)
+  const Emb = new Discord.RichEmbed()
+      .setImage(body.url);
 	let mention = message.mentions.users.first();
-    if (!mention) return message.channel.send(`Bạn phải chọn người để hôn đã :|`).then(msg => {
+  if (!mention) return message.channel.send(`Bạn phải chọn người để hôn đã :|`).then(msg => {
     	msg.delete(3000)
-  	});
-    else if(args[0] === '<@514823687560757268>') message.channel.send(`Aww cảm ơn bạn XD ${body.url}`);	  
-    else message.channel.send(`**${message.author.username}** vừa hôn **${mention.username}** kìa :kissing_heart: ${body.url}`);
+  });
+  else if(args[0] === '<@514823687560757268>') message.channel.send(`Aww cảm ơn bạn XD`,{embed: Emb});	  
+  else message.channel.send(`**${message.author.username}** vừa hôn **${mention.username}** kìa :kissing_heart: `,{embed: Emb});
 
 }
 
 module.exports.config ={
-	command: 'kiss'
+	command: 'kiss',
+  	category: 'Actions',
+  	description: 'Dùng để hôn ai đó XD',
+  	usage: '>>kiss [@user]'
 }
