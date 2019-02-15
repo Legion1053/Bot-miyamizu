52d7e74cca79c6cc95246c489a2f12d6bd2388ea
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-02-15 11:07:36 +0700

    Update leave.js

--- cmds/Music/leave.js
+++ cmds/Music/leave.js
@@ -1,9 +1,15 @@
-module.exports.run = async(bot,message,args)=>{
-	if(!message.member.voiceChannel) return message.channel.send('Bạn phải vào voice channel trước đã!');
-	if(!message.guild.me.voiceChannel) return message.channel.send('Bot hiện tại đang không ở trong channel');
-	message.member.voiceChannel.leave();
+module.exports.run = async(bot,message,args,ops) =>{
+  if(!message.member.voiceChannel) return message.channel.send('Bạn phải vào voice channel trước đã!');
+	if(!message.guild.me.voiceChannel) return message.channel.send('Bot hiện tại không ở trong voice channel');
+	if(message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send('Bạn đang không ở chung phòng với bot!');
+
+	message.guild.me.voiceChannel.leave();
 	message.channel.send("Đã rời khỏi voice Channel");	
 }
+
 module.exports.config = {
-	command: "leave"
+   command: 'leave',
+   category: 'Music',
+   description: "Dùng để yêu cầu bot ra khỏi voice channel",
+   usage: ">>leave"
 }
