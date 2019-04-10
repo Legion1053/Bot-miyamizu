c22717420359c9df50c61faa05855fadcbd05c97
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-04-10 09:52:38 +0700

    Update daily.js

--- cmds/Economy/daily.js
+++ cmds/Economy/daily.js
@@ -1,47 +1,54 @@
 const Discord = require('discord.js');
-const moment = require('moment');
-const db = require('quick.db');
+const mongoose = require('mongoose');
 const ms = require('parse-ms');
 
-module.exports.run = async(bot,message,args)=>{
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}/${process.env.DB}`,{
+  useNewUrlParser: true
+});
+
+module.exports.run = async(bot,message,args,ops,coin)=>{
 	let cooldown = 8.64e+7;
   let daily = 250;
-  
-  let lastDaily = await db.fetch(`lastDaily_${message.author.id}`);
-    try {
-    db.fetch(`userBalance_${message.member.id}`).then(bal => {
-    if(bal == null||0){
-        db.set(`userBalance_${message.member.id}`, 50)}
-
-    else if (lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0) {
-        let timeObj = ms(cooldown - (Date.now() - lastDaily));
-        console.log(Date.now() - lastDaily);
-
-        let lastDailyEmbed = new Discord.RichEmbed()
-          .setAuthor(`Lần nhận thưởng tiếp theo`)
-          .setColor('#ffff00')
-          .setDescription(`Bạn chỉ có thể nhận được tiền thưởng trong: **${timeObj.hours} giờ ${timeObj.minutes} phút** nữa!`)
-          .setFooter(message.author.tag)
-        message.channel.send(lastDailyEmbed);
-    } else {
-        db.set(`lastDaily_${message.author.id}`, Date.now());
-        db.add(`userBalance_${message.member.id}`, daily).then(i => {
-          let embed = new Discord.RichEmbed()
-          .setTitle('Tiền thưởng hằng ngày')
-          .setDescription(`Bạn đã nhận được 250$ từ tiền thưởng mỗi ngày`)
-          .setColor('#ffff00')
-          .setFooter(message.author.tag)
-          message.channel.send(embed);
-        })}
-    })} 
-    catch(err) {
-        console.log(err)
-    }
+  try {
+  coin.findOne({
+            userID: message.author.id,
+        },(err,res) =>{
+            if(err) console.log(err);
+            if(!res){
+            const newDoc = new coin({
+                userID: message.author.id,
+                steak: 1,
+                money: 200,
+                lastDaily: Date.now()
+            })
+        newDoc.save().catch(err => console.log(err));
+        } else if (res.lastDaily !== null && cooldown - (Date.now() - res.lastDaily) > 0) {
+            let timeObj = ms(cooldown - (Date.now() - res.lastDaily));
+            let lastDailyEmbed = new Discord.RichEmbed()
+                .setAuthor(`Lần nhận thưởng tiếp theo`)
+                .setColor('#ffff00')
+                .setDescription(`Bạn chỉ có thể nhận được tiền thưởng trong: **${timeObj.hours} giờ ${timeObj.minutes} phút** nữa!`)
+                .setFooter(message.author.tag)
+            message.channel.send(lastDailyEmbed);
+        } else {
+            res.lastDaily = Date.now();
+            res.money += daily;
+            let embed = new Discord.RichEmbed()
+                .setTitle('Tiền thưởng hằng ngày')
+                .setDescription(`Bạn đã nhận được 250 đồng từ tiền thưởng mỗi ngày`)
+                .setColor('#ffff00')
+                .setFooter(message.author.tag)
+            message.channel.send(embed);
+        }
+      })
+  } catch(err){
+      console.error(err);
+  }
 }
 
 module.exports.config = {
 	command: 'daily',
- 	category: 'Economy',
-  	description: "Dùng để đưa bạn 250$ mỗi ngày",
-  	usage: ">>ping"
+  category: 'Economy',
+  description: "Dùng để đưa bạn 250đ mỗi ngày",
+  usage: ">>ping"
 }
