1cf6b9f904920b3ee126f01009169b3c85b695d3
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-04-23 09:41:30 +0700

    Update daily.js

--- cmds/Economy/daily.js
+++ cmds/Economy/daily.js
@@ -2,15 +2,11 @@ const Discord = require('discord.js');
 const mongoose = require('mongoose');
 const ms = require('parse-ms');
 
-mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}/${process.env.DB}`,{
-  useNewUrlParser: true
-});
-
 module.exports.run = async(bot,message,args,ops,coin)=>{
 	let cooldown = 8.64e+7;
-  let daily = 250;
-  try {
-  coin.findOne({
+  	let daily = 250;
+  	try {
+  	coin.findOne({
             userID: message.author.id,
         },(err,res) =>{
             if(err) console.log(err);
@@ -18,10 +14,20 @@ module.exports.run = async(bot,message,args,ops,coin)=>{
             const newDoc = new coin({
                 userID: message.author.id,
                 steak: 1,
-                money: 200,
+                money: 1000,
                 lastDaily: Date.now()
-            })
+        })
         newDoc.save().catch(err => console.log(err));
+        
+        res.lastDaily = Date.now();
+        res.money += daily;
+            let embed = new Discord.RichEmbed()
+                .setTitle('Tiền thưởng hằng ngày')
+                .setDescription(`Bạn đã nhận được 250đ từ tiền thưởng mỗi ngày`)
+                .setColor('#ffff00')
+                .setFooter(message.author.tag)
+            message.channel.send(embed);
+        res.save().catch(err => console.error(err));
         } else if (res.lastDaily !== null && cooldown - (Date.now() - res.lastDaily) > 0) {
             let timeObj = ms(cooldown - (Date.now() - res.lastDaily));
             let lastDailyEmbed = new Discord.RichEmbed()
@@ -35,10 +41,11 @@ module.exports.run = async(bot,message,args,ops,coin)=>{
             res.money += daily;
             let embed = new Discord.RichEmbed()
                 .setTitle('Tiền thưởng hằng ngày')
-                .setDescription(`Bạn đã nhận được 250 đồng từ tiền thưởng mỗi ngày`)
+                .setDescription(`Bạn đã nhận được **250**đ từ tiền thưởng mỗi ngày`)
                 .setColor('#ffff00')
                 .setFooter(message.author.tag)
             message.channel.send(embed);
+          res.save().catch(err => console.error(err));
         }
       })
   } catch(err){
@@ -48,7 +55,8 @@ module.exports.run = async(bot,message,args,ops,coin)=>{
 
 module.exports.config = {
 	command: 'daily',
-  category: 'Economy',
-  description: "Dùng để đưa bạn 250đ mỗi ngày",
-  usage: ">>ping"
+ 	category: 'Economy',
+  	description: "Dùng để đưa bạn 250đ mỗi ngày",
+  	usage: ">>daily",
+  	aliases: []
 }
