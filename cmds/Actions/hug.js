ed91c90eff780b1c67999830ded66b9078bb7d59
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-04-23 09:33:53 +0700

    Update hug.js

--- cmds/Actions/hug.js
+++ cmds/Actions/hug.js
@@ -18,5 +18,6 @@ module.exports.config = {
 	command: 'hug',
   	category: 'Actions',
   	description: 'Dùng để ôm ai đó <3',
-  	usage: '>>hug [@user]'
+  	usage: '>>hug [@user]',
+  	aliases: []
 }
