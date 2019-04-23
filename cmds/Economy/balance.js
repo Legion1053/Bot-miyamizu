053e513d8a1fd0668993f917dd7a5e9bcf9dd371
Legion1053 <45529714+Legion1053@users.noreply.github.com>
2019-04-23 09:40:25 +0700

    Update balance.js

--- cmds/Economy/balance.js
+++ cmds/Economy/balance.js
@@ -1,38 +1,31 @@
 const Discord = require('discord.js');
-const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}/${process.env.DB}`,{
-  useNewUrlParser: true
-});
 
 module.exports.run = async(bot,message,args,ops,coin) => {
-      let mention = message.mentions.users.first() || message.author;
-  
+   let mention = message.mentions.users.first() || message.author;
+      try{
       coin.findOne({
             userID: mention.id,
         },(err,res) =>{
-            if(err) console.log(err);
-            if(!res){
-            const newDoc = new coin({
-                userID: mention.id,
-                steak: 1,
-                money: 200,
-                lastDaily: 0
-            })
-        newDoc.save().catch(err => console.log(err));
-        } else {
+            if(err) {
+              console.log(err);
+              return message.channel.send('Nguời này chưa khởi tạo profile!');
+            }
             let embed = new Discord.RichEmbed()
                 .setTitle('Tổng số tiền')
                 .setDescription(`${mention.username}, bạn có **${res.money}**đ trong tài khoản`)
                 .setColor('#ffff00')
-            message.channel.send(embed)
-        }
-      })
+        return message.channel.send(embed)
+        })
+   } catch(err) {
+          throw err;
+          return message.channel.send('Nguời này chưa khởi tạo profile!');
+   }
 }
 
 module.exports.config = {
   command: 'balance',
   category: 'Economy',
   description: "Dùng để kiểm tra số tiền hiện tại của mình, hoặc của người khác",
-  usage: ">>balance [@user]"
+  usage: ">>balance [@user]",
+  aliases: ['bal','money']
 }
